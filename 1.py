14282eb61b4d51b6f22c308e49148c1900c2be31
Jurij Procenko <elsoul@gmail.com>
2023-10-01 23:14:25 +0300

    Deleted wrong file

--- 1.py
+++
@@ -1,4 +0,0 @@
-# Store people's favorite languages. 
-fav_languages = { 'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python', }
-for name in sorted(fav_languages.keys()): 
-    print(name + ": " + fav_languages[name])
