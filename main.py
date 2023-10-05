c492621953d031f87b977b3caaca745d09b01041
Jurij Procenko <elsoul@gmail.com>
2023-10-05 10:04:28 +0300

    Make unification function's call

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ help                         - this information
 
 from pathlib import Path
 
+data_pb = Path("phonebook.txt")
+phone_book = {}
+if data_pb.exists():
+    with open(data_pb, "r") as pb:
+        records = pb.readlines()
+        for record in records:
+            record = record.replace("\n", "").lower().split()
+            phone_book[" ".join(record[:-1])] = record[-1]
+
 
 def input_error(func):
     def inner(*args, **kwargs):
@@ -37,85 +46,81 @@ def normalize(number: str) -> str:
 
 
 @input_error
-def add_number(person: str, number: str, phone_book: dict) -> tuple:
-    phone_book[person] = normalize(number)
-    return "Abonent added succefully!"
+def add_number(*args) -> str:
+    person = " ".join(args[:-1])
+    phone_book[person] = normalize(args[-1])
+    return f"Abonent {person} with number {phone_book[person]} added succefully!"
 
 
 @input_error
-def change_number(person: str, number: str, phone_book: dict) -> tuple:
+def change_number(*args) -> str:
+    person = " ".join(args[:-1])
     if person not in phone_book:
         raise KeyError
-    phone_book[person] = normalize(number)
-    return f"Phone number <{person}> changed succefully!"
+    phone_book[person] = normalize(args[-1])
+    return f"Phone number <{person}> changed succefully to {phone_book[person]}!"
 
 
 @input_error
-def phone(person: str, **phone_book: dict) -> str:
-    return phone_book[person]
+def phone(*args) -> str:
+    person = " ".join(args)
+    return f"{person} {phone_book[person]}"
 
 
 @input_error
-def delete(person: str, phone_book: dict) -> tuple:
+def delete(*args) -> str:
+    person = " ".join(args)
     del phone_book[person]
     return f"Abonent <{person}> was succefully deleted!"
 
-
-def show_all(phone_book: dict) -> str:
+@input_error
+def show_all() -> str:
     return_string = ""
     for key, values in phone_book.items():
         return_string += f"{key}    {values}\n"
     return return_string
 
+@input_error
+def help(*args):
+    return __doc__
 
-def parser(command: str, phone_book: dict, data_pb: Path) -> str:
-    if command.startswith("help"):
-        return __doc__
+@input_error
+def hello(*args):
+    return "How can I help you?"
 
-    if command.startswith("show all"):
-        return show_all(phone_book)
 
-    if command.startswith(("good bye", "close", "exit")):
+COMMANDS = {
+    "add": add_number,
+    "change": change_number,
+    "show all": show_all,
+    "phone": phone,
+    "delete": delete,
+    "hello": hello,
+    "help": help,
+}
+
+def parser(command: str) -> str:
+    if command.lower().startswith("show all"):
+        return show_all()
+
+    if command.lower().startswith(("good bye", "close", "exit")):
         with open(data_pb, "w") as pb:
             for k, v in phone_book.items():
                 pb.write(f"{k} {v}\n")
         return "Good bye!"
 
-    command = command.lower().split()
-    match command[0]:
-        case "hello":
-            return "How can I help you?"
-        case "phone":
-            return phone(" ".join(command[1:]), **phone_book)
-        case "add":
-            retcode = add_number(" ".join(command[1:-1]), command[-1], phone_book)
-            return_str = retcode
-            return return_str
-        case "change":
-            retcode = change_number(" ".join(command[1:-1]), command[-1], phone_book)
-            return_str = retcode
-            return return_str
-        case "delete":
-            retcode = delete(" ".join(command[1:]), phone_book)
-            return_str = retcode
-            return return_str
+    command = command.split()
+    command[0] = command[0].lower()
+    if command[0] in COMMANDS:
+        return COMMANDS[command[0]](*command[1:])
 
     return "Command not recognized, try again"
 
 
 def main():
-    data_pb = Path("phonebook.txt")
-    phone_book = {}
-    if data_pb.exists():
-        with open(data_pb, "r") as pb:
-            records = pb.readlines()
-            for record in records:
-                record = record.replace("\n", "").lower().split()
-                phone_book[" ".join(record[:-1])] = record[-1]
-
     while True:
         command = input("Enter your command>> ")
-        ret_code = parser(command, phone_book, data_pb)
+        ret_code = parser(command)
         if ret_code == "Good bye!":
             print("Good bye!")
             break
